3cda3f76d5e7bf0eeb25bdf98fc63ef47877e398
christmo99 <christmo99@9b2dfb06-ce61-f010-6159-ca073f73f5a6>
2011-05-03 21:39:03 +0000

    Conexion a la base

---
+++ SistemaRecomendadorLibros/src/BaseDatos/BaseDatos.java
@@ -0,0 +1,148 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaseDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author kradac
+ */
+public class BaseDatos {
+
+    private Connection conexion;
+    /**
+     * Nombre de la Base de datos
+     */
+    private String bd = "recomendador";
+    private String ip = "localhost";
+    private String usr = "root";
+    private String pass = "";
+    private ResultSet rs = null;
+
+    public BaseDatos() {
+        ConexionBase();
+    }
+
+    /**
+     * Crea una conexion a cualquier base de datos mysql, con parametros
+     * de conexion indepenientes
+     * @param ip - IP del servidor
+     * @param bd - Nombre de la Base de datos
+     * @param usr -  Nombre de Usuario
+     * @param pass - Clave de la Base de datos
+     */
+    private void ConexionBase() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://" + ip + "/" + bd;
+        try {
+            try {
+                Class.forName(driver).newInstance();
+            } catch (InstantiationException ex) {
+                java.util.logging.Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalAccessException ex) {
+                java.util.logging.Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(BaseDatos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conexion = DriverManager.getConnection(url, usr, pass);
+        } catch (SQLException ex) {
+            System.out.println("No se puede obtener la conexion...");
+        }
+
+        System.out.println("Conexion a Base de Datos OK: " + bd);
+    }
+
+    /**
+     * Ejecuta una consulta en la base de datos, que devuelve valores
+     * es necesario recorrer el resultset
+     * @param sql - debe ser Select
+     * @return ResultSet
+     */
+    public ResultSet ejecutarConsulta(String sql) {
+        ResultSet r = null;
+        try {
+            Statement stat = (Statement) conexion.createStatement();
+            r = stat.executeQuery(sql);
+            System.out.println("SQL:" + sql);
+        } catch (SQLException ex) {
+            System.out.println("No se pudo completar la consulta...");
+        } catch (NullPointerException ex) {
+        }
+        return r;
+    }
+
+    /**
+     * Ejecuta una consulta en la base de datos, que devuelve valores
+     * no es necesario recorrer el resultset
+     * @param sql - debe ser Select
+     * @return ResultSet
+     */
+    public ResultSet ejecutarConsultaUnDato(String sql) {
+        ResultSet rsCUD = null;
+        try {
+            Statement sta = (Statement) conexion.createStatement();
+            rsCUD = sta.executeQuery(sql);
+            System.out.println("SQL:" + sql);
+            rsCUD.next();
+        } catch (SQLException ex) {
+            System.out.println("No se pudo completar la consulta...");
+        }
+        return rsCUD;
+    }
+
+    /**
+     * Ejecuta una sentencia en la base esta puede ser de INSERT, UPDATE O
+     * DELETE
+     * @param sql - Sentencias INSERT, UPDATE, DELETE
+     * @return int - confirmacion del resultado 1 valido || 0 invalido
+     */
+    public boolean ejecutarSentencia(String sql) {
+        try {
+            Statement st = (Statement) conexion.createStatement();
+
+            System.out.println("Ejecutar: " + sql);
+
+            int rta = st.executeUpdate(sql);
+            if (rta >= 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException ex) {
+            System.out.println("No se pudo completar la ejecución...");
+            return false;
+        } catch (NullPointerException ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Cierra la conexion con la base de datos
+     * @return Connection
+     * @throws SQLException
+     */
+    public void CerrarConexion() {
+        try {
+            conexion.close();
+        } catch (SQLException ex) {
+            System.out.println("No se pudo completar el cierre de la BD...");
+        } catch (NullPointerException ex) {
+            System.out.println("NO está abierta la base de datos...");
+        }
+    }
+}
---
+++ SistemaRecomendadorLibros/src/BaseDatos/sbc.sql
@@ -0,0 +1,179 @@
+-- MySQL dump 10.13  Distrib 5.1.36, for Win32 (ia32)
+--
+-- Host: localhost    Database: recomendador
+-- ------------------------------------------------------
+-- Server version	5.1.36-community-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+create database recomendador;
+use recomendador;
+
+--
+-- Table structure for table `autores`
+--
+
+DROP TABLE IF EXISTS `autores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `autores` (
+  `idAutor` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `idPais` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idAutor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `autores`
+--
+
+LOCK TABLES `autores` WRITE;
+/*!40000 ALTER TABLE `autores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `autores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `categorias`
+--
+
+DROP TABLE IF EXISTS `categorias`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categorias` (
+  `idCategoria` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias`
+--
+
+LOCK TABLES `categorias` WRITE;
+/*!40000 ALTER TABLE `categorias` DISABLE KEYS */;
+INSERT INTO `categorias` VALUES (1,'Poesía'),(2,'Historia'),(3,'Sociología'),(4,'Psicología'),(5,'Ciencias'),(6,'Política'),(7,'Novelas');
+/*!40000 ALTER TABLE `categorias` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `idClientes` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `idIdioma` int(10) unsigned NOT NULL,
+  `anio` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idClientes`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `idiomas`
+--
+
+DROP TABLE IF EXISTS `idiomas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `idiomas` (
+  `idIdioma` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `idioma` varchar(50) NOT NULL,
+  PRIMARY KEY (`idIdioma`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `idiomas`
+--
+
+LOCK TABLES `idiomas` WRITE;
+/*!40000 ALTER TABLE `idiomas` DISABLE KEYS */;
+INSERT INTO `idiomas` VALUES (1,'Español'),(2,'Ingles'),(3,'Alemán'),(4,'Portugués'),(5,'Italianos'),(6,'Francés'),(7,'Chino'),(8,'Coreano'),(9,'Japones');
+/*!40000 ALTER TABLE `idiomas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `libros`
+--
+
+DROP TABLE IF EXISTS `libros`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `libros` (
+  `idLibro` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(100) NOT NULL,
+  `precio` double NOT NULL,
+  `resumen` varchar(500) NOT NULL,
+  `editorial` varchar(100) NOT NULL,
+  `bestseller` tinyint(1) NOT NULL,
+  `idIdima` int(10) unsigned NOT NULL,
+  `idPais` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`idLibro`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `libros`
+--
+
+LOCK TABLES `libros` WRITE;
+/*!40000 ALTER TABLE `libros` DISABLE KEYS */;
+/*!40000 ALTER TABLE `libros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paises`
+--
+
+DROP TABLE IF EXISTS `paises`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paises` (
+  `idPais` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `pais` varchar(50) NOT NULL,
+  PRIMARY KEY (`idPais`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paises`
+--
+
+LOCK TABLES `paises` WRITE;
+/*!40000 ALTER TABLE `paises` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paises` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2011-05-03 14:57:05
