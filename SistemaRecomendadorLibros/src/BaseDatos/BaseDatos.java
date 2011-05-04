2ef9568168eefba23d0253daab9c5843c2efc2ff
qmarqeva <qmarqeva@9b2dfb06-ce61-f010-6159-ca073f73f5a6>
2011-05-04 16:27:18 +0000

    Esquema BD modificado

--- SistemaRecomendadorLibros/src/BaseDatos/BaseDatos.java
+++ SistemaRecomendadorLibros/src/BaseDatos/BaseDatos.java
@@ -9,12 +9,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
